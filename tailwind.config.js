fe4a665535a2d9486856b7b084beca7cb17535b5
Liam Chalder <liam.chalder@laser.red>
2023-04-17 11:55:12 +0100

    :zap: update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './storage/framework/views/*.php',
+      './resources/views/*.blade.php',
+      './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
