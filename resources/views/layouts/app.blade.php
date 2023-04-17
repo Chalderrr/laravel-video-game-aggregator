143b68c61f116ebf0cf56d773d1ed91820730ff9
Liam Chalder <liam.chalder@laser.red>
2023-04-17 16:51:33 +0100

    :lipstick: set up markup for api

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-800 text-white">
+<body class="bg-gray-900 text-white">
     <header class="border-b border-gray-800">
         <nav class="container mx-auto flex items-center justify-between px-4 py-6">
             <div class="flex items-center">
@@ -25,7 +25,12 @@
             </div>
             <div class="flex items-center">
                 <div class="relative">
-
+                    <input type="text" name="" id="" class="bg-gray-800 text-sm rounded-full pl-8 pr-3 py-1.5 w-64" placeholder="Search..." aria-label="Search">
+                    <div class="absolute top-0 left-2 flex items-center h-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </div>
                 </div>
                 <div class="ml-6">
                     <a href="#"><img src="{{ asset('avatar.jpg') }}" alt="" class="rounded-full w-8"></a>
@@ -33,5 +38,15 @@
             </div>
         </nav>
     </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+<footer class="border-t border-gray-800">
+    <div class="container mx-auto px-4 py-6">
+        Powered By <a href="#" class="underline hover:no-underline">IGDB API</a>
+    </div>
+</footer>
 </body>
 </html>
