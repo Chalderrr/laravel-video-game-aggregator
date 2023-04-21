6f1e0dc5e34d052867f37648cbfc82874cb87437
Liam Chalder <liam.chalder@laser.red>
2023-04-21 12:31:48 +0100

    :zap: implement a Cache::remember for faster loading

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -16,15 +17,17 @@ class PopularGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Http::withHeaders(config('services.igdb.headers'))
-            ->withBody(
-                "fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count;
-                where platforms = (48,49,130,6)
-                & (first_release_date >= '{$before}'
-                & first_release_date < '{$after}');
-                limit 20;"
-            )->post(config('services.igdb.endpoint'))
-            ->json();
+        $this->popularGames = Cache::remember('popular-games', 7, function () use ($before, $after) {
+            return Http::withHeaders(config('services.igdb.headers'))
+                ->withBody(
+                    "fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count;
+                    where platforms = (48,49,130,6)
+                    & (first_release_date >= '{$before}'
+                    & first_release_date < '{$after}');
+                    limit 20;"
+                )->post(config('services.igdb.endpoint'))
+                ->json();
+        });
     }
 
     public function render()
