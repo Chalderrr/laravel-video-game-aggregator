18d1297e822043736904ee25735221ac77fd02ce
Liam Chalder <liam.chalder@laser.red>
2023-06-21 10:48:27 +0100

    :zap: update PopularGames to output what we need instead of amending in the blade.

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class PopularGames extends Component
@@ -17,7 +18,7 @@ class PopularGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Cache::remember('popular-games', 7, function () use ($before, $after) {
+        $popularGamesUnformatted = Cache::remember('popular-games', 7, function () use ($before, $after) {
             return Http::withHeaders(config('services.igdb.headers'))
                 ->withBody(
                     "fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count, slug;
@@ -28,10 +29,24 @@ class PopularGames extends Component
                 )->post(config('services.igdb.endpoint'))
                 ->json();
         });
+
+
+        $this->popularGames = $this->formatForView($popularGamesUnformatted);
     }
 
     public function render()
     {
         return view('livewire.popular-games');
+    }
+
+    public function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' =>  array_key_exists( 'cover', $game ) ? Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) : '',
+                'rating' => isset($game['rating']) ? round($game['rating']).'%' : null,
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->filter()->implode(', ')
+            ])->toArray();
+        });
     }
 }
